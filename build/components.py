20eef3f3932586f22f49ae797740d70db40871ce
Todd Roper <toddroper@me.com>
2023-08-23 12:11:28 -0700

    Adding component file.

---
+++ build/components.py
@@ -0,0 +1,30 @@
+from yaml import safe_load, dump, Dumper
+from pathlib import Path
+from ruamel.yaml import YAML
+from build import get_base_yaml
+
+
+class AskemDumper(Dumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(AskemDumper, self).increase_indent(flow, False)
+
+
+def extract_components(file_path: str, component_dir: str):
+    yaml = YAML()
+    yaml_file = yaml.load(Path(file_path))
+
+    for service in yaml_file["services"]:
+        save_service_file(
+            name=service,
+            component_dir=component_dir,
+            service_definition=yaml_file["services"][service],
+        )
+
+
+def save_service_file(name: str, component_dir: str, service_definition: dict):
+    print(f"Saving {name} service.")
+    component_file = f"{component_dir}/{name}.yml"
+    with open(component_file, "w") as cfile:
+        yaml = get_base_yaml()
+        dump_obj = {f"{name}": service_definition}
+        yaml.dump(dump_obj, cfile)
